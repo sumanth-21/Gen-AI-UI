59382c96443b0d070f04e92b1c01e8f87c7b0cf2
sumanth-21 <saisumanthgoud.p@gmail.com>
2024-07-09 17:55:00 -0400

    Initial project setup

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from "react";
+import "./App.css";
+
+import { Button } from "@material-tailwind/react";
+
+function App() {
+  return (
+    <>
+      <div>
+        <Button>Button</Button>
+      </div>
+    </>
+  );
+}
+
+export default App;
